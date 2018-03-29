bbacc0d5d4eef9e31e2737ce65d79f7b293e2321
yvan vander sanden <yvan@attr-x.net>
2018-03-29 22:25:10 +0200

    Forgotten Changes

--- InteractClient/InteractClient/InteractClient/JintEngine/Engine.cs
+++ InteractClient/InteractClient/InteractClient/JintEngine/Engine.cs
@@ -10,214 +10,215 @@ using Xamarin.Forms;
 
 namespace InteractClient.JintEngine
 {
-  class Engine
-  {
-    public static Engine Instance = new Engine();
-    public static SemaphoreSlim ConstructionLock;
-
-    private bool active = false;
-    public bool Active { get => active; }
-
-    private ContentPage activePage;
-
-    private bool ScreenNeedsToStart = false;
-    private int screenToStart;
-
-    // Jint Engine
-    private Jint.Engine jEngine = null;
-    private Implementation.Values Values  = new Implementation.Values();
-    private Implementation.Network.Server Server = new Implementation.Network.Server();
-    private Implementation.Device.Sensors Sensors;
-    private Implementation.Device.Arduino Arduino;
-
-    public Jint.Engine Jint()
+    class Engine
     {
-      if (jEngine == null)
-      {
-        jEngine = new Jint.Engine();
-        jEngine.SetValue("Root", (activePage as ModelPage).PageRoot);
-        jEngine.SetValue("Values", Values);
+        public static Engine Instance = new Engine();
+        public static SemaphoreSlim ConstructionLock;
 
-        if (Sensors == null)
+        private bool active = false;
+        public bool Active { get => active; }
+
+        private ContentPage activePage;
+
+        private bool ScreenNeedsToStart = false;
+        private int screenToStart;
+
+        // Jint Engine
+        private Jint.Engine jEngine = null;
+        private Implementation.Values Values = new Implementation.Values();
+        private Implementation.Network.Server Server = new Implementation.Network.Server();
+        private Implementation.Device.Sensors Sensors;
+        private Implementation.Device.Arduino Arduino;
+
+        public Jint.Engine Jint()
         {
-          try
-          {
-            //Sensors = new Implementation.Device.Sensors();
-            //jEngine.SetValue("Sensors", Sensors);
-          }
-          catch (Exception e)
-          {
-            Network.Signaler.Get().WriteLog("Can't setup sensors:" + e.Message);
-          }
+            if (jEngine == null)
+            {
+                jEngine = new Jint.Engine();
+                jEngine.SetValue("Root", (activePage as ModelPage).PageRoot);
+                jEngine.SetValue("Values", Values);
+
+                if (Sensors == null)
+                {
+                    try
+                    {
+                        //Sensors = new Implementation.Device.Sensors();
+                        //jEngine.SetValue("Sensors", Sensors);
+                    }
+                    catch (Exception e)
+                    {
+                        Network.Signaler.Get().WriteLog("Can't setup sensors:" + e.Message);
+                    }
+                }
+
+                if (Arduino == null)
+                {
+                    Arduino = new Implementation.Device.Arduino();
+                }
+
+                if (Arduino.IsImplemented())
+                {
+                    Arduino.Start();
+                    jEngine.SetValue("Arduino", Arduino);
+                }
+
+                // UI
+                jEngine.SetValue("Button", TypeReference.CreateTypeReference(jEngine, typeof(Implementation.UI.Button)));
+                jEngine.SetValue("Title", TypeReference.CreateTypeReference(jEngine, typeof(Implementation.UI.Title)));
+                jEngine.SetValue("Text", TypeReference.CreateTypeReference(jEngine, typeof(Implementation.UI.Text)));
+                jEngine.SetValue("Slider", TypeReference.CreateTypeReference(jEngine, typeof(Implementation.UI.Slider)));
+                jEngine.SetValue("Image", TypeReference.CreateTypeReference(jEngine, typeof(Implementation.UI.Image)));
+                jEngine.SetValue("Grid", TypeReference.CreateTypeReference(jEngine, typeof(Implementation.UI.Grid)));
+
+
+                jEngine.SetValue("Color", TypeReference.CreateTypeReference(jEngine, typeof(Implementation.UI.Color)));
+
+                // Network
+                jEngine.SetValue("Server", Server);
+                jEngine.SetValue("Clients", Project.Current.Clients);
+                jEngine.SetValue("OscSender", TypeReference.CreateTypeReference(jEngine, typeof(Implementation.Network.OscSender)));
+                jEngine.SetValue("OscReceiver", TypeReference.CreateTypeReference(jEngine, typeof(Implementation.Network.OscReceiver)));
+                jEngine.SetValue("Timer", TypeReference.CreateTypeReference(jEngine, typeof(Implementation.Logic.Timer)));
+
+                jEngine.SetValue("PinMode", TypeReference.CreateTypeReference(jEngine, typeof(Interact.Device.Arduino.PinMode)));
+                jEngine.SetValue("PinState", TypeReference.CreateTypeReference(jEngine, typeof(Interact.Device.Arduino.PinState)));
+            }
+            return jEngine;
         }
 
-        if(Arduino == null)
+        public void StartScreen(int ID)
         {
-          Arduino = new Implementation.Device.Arduino(); 
+            if (ConstructionLock == null) ConstructionLock = new SemaphoreSlim(1);
+            ConstructionLock.Wait(5000);
+
+            Arduino?.AllowJintOutput(false);
+            Arduino?.RemoveSignalHandlers();
+
+            if (activePage is ConnectedPage)
+            {
+                ScreenNeedsToStart = true;
+                screenToStart = ID;
+                ConnectedPage page = activePage as ConnectedPage;
+                Device.BeginInvokeOnMainThread(() => page.PushModelPage());
+                InteractClient.Network.Signaler.Get().GetNextMethod();
+
+            }
+            else if (activePage is ModelPage)
+            {
+
+                screenToStart = ID;
+                ModelPage p = activePage as ModelPage;
+                string screenName = Data.Project.Current.GetScreen(ID).Name;
+                Device.BeginInvokeOnMainThread(() => p.StartScript(screenName));
+            }
         }
 
-        if (Arduino.IsImplemented())
+        public void StopScreen()
         {
-          Arduino.Start();
-          jEngine.SetValue("Arduino", Arduino);
+            if (activePage is ModelPage)
+            {
+                active = false;
+                ModelPage p = activePage as ModelPage;
+                Device.BeginInvokeOnMainThread(() => p.Pop());
+                jEngine = null;
+                Arduino?.RemoveSignalHandlers();
+            }
+            InteractClient.Network.Signaler.Get().GetNextMethod();
         }
 
-        // UI
-        jEngine.SetValue("Button", TypeReference.CreateTypeReference(jEngine, typeof(Implementation.UI.Button)));
-        jEngine.SetValue("Title", TypeReference.CreateTypeReference(jEngine, typeof(Implementation.UI.Title)));
-        jEngine.SetValue("Text", TypeReference.CreateTypeReference(jEngine, typeof(Implementation.UI.Text)));
-        jEngine.SetValue("Slider", TypeReference.CreateTypeReference(jEngine, typeof(Implementation.UI.Slider)));
-        jEngine.SetValue("Image", TypeReference.CreateTypeReference(jEngine, typeof(Implementation.UI.Image)));
-        jEngine.SetValue("Grid", TypeReference.CreateTypeReference(jEngine, typeof(Implementation.UI.Grid)));
-        
-        
-        jEngine.SetValue("Color", TypeReference.CreateTypeReference(jEngine, typeof(Implementation.UI.Color)));
-
-        // Network
-        jEngine.SetValue("Server", Server);
-        jEngine.SetValue("Clients", Project.Current.Clients);
-        jEngine.SetValue("OscSender", TypeReference.CreateTypeReference(jEngine, typeof(Implementation.Network.OscSender)));
-        jEngine.SetValue("OscReceiver", TypeReference.CreateTypeReference(jEngine, typeof(Implementation.Network.OscReceiver)));
-        jEngine.SetValue("Timer", TypeReference.CreateTypeReference(jEngine, typeof(Implementation.Logic.Timer)));
-
-        jEngine.SetValue("PinMode", TypeReference.CreateTypeReference(jEngine, typeof(Interact.Device.Arduino.PinMode)));
-        jEngine.SetValue("PinState", TypeReference.CreateTypeReference(jEngine, typeof(Interact.Device.Arduino.PinState)));
-      }
-      return jEngine;
-    }
-
-    public void StartScreen(int ID)
-    {
-      if (ConstructionLock == null) ConstructionLock = new SemaphoreSlim(1);
-      ConstructionLock.Wait(5000);
-
-      Arduino?.AllowJintOutput(false);
-      Arduino?.RemoveSignalHandlers();
-
-      if (activePage is ConnectedPage)
-      {
-        ScreenNeedsToStart = true;
-        screenToStart = ID;
-        ConnectedPage page = activePage as ConnectedPage;
-        Device.BeginInvokeOnMainThread(() => page.PushModelPage());
-        InteractClient.Network.Signaler.Get().GetNextMethod();
-
-      }
-      else if (activePage is ModelPage)
-      {
-        
-        screenToStart = ID;
-        ModelPage p = activePage as ModelPage;
-        string screenName = Data.Project.Current.GetScreen(ID).Name;
-        Device.BeginInvokeOnMainThread(() => p.StartScript(screenName));
-      }
-    }
-
-    public void StopScreen()
-    {
-      if (activePage is ModelPage)
-      {
-        active = false;
-        ModelPage p = activePage as ModelPage;
-        Device.BeginInvokeOnMainThread(() => p.Pop());
-        jEngine = null;
-        Arduino?.RemoveSignalHandlers();
-      }
-      InteractClient.Network.Signaler.Get().GetNextMethod();
-    }
-
-    public void StopRunningProject()
-    {
-      active = false;
-      Sensors?.Stop();
-      Arduino?.Stop();
-      StopScreen();
-    }
-
-    public void SetActivePage(ContentPage page)
-    {
-      activePage = page;
-      if (page is ModelPage && ScreenNeedsToStart)
-      {
-        StartScript(screenToStart);
-        ScreenNeedsToStart = false;
-      }
-    }
-
-    public void StartScript(int ID)
-    {
-      Screen screen = Project.Current.GetScreen(ID);
-
-      if (screen == null)
-      {
-        InteractClient.Network.Signaler.Get().WriteLog("Engine->StartScript: screen " + ID + " not found.");
-        ConstructionLock.Release();
-        return;
-      }
-      try
-      {
-
-        Jint().Execute(screen.screenContent.Content);
-        Jint().Invoke("Init");
-        InteractClient.Network.Signaler.Get().WriteLog("Engine->StartScript: screen " + ID + " is now running.");
-      }
-      catch (Jint.Parser.ParserException e)
-      {
-        InteractClient.Network.Signaler.Get().WriteErrorLog(e.Index, e.LineNumber, e.Message, ID);
-        ConstructionLock.Release();
-        return;
-      }
-      catch (Jint.Runtime.JavaScriptException e)
-      {
-        InteractClient.Network.Signaler.Get().WriteErrorLog(0, e.LineNumber, e.Message, ID);
-        ConstructionLock.Release();
-        return;
-      }
-      catch (Exception e)
-      {
-        InteractClient.Network.Signaler.Get().WriteLog("Engine->StartScript: Error on screen " + ID + ":" + e.Message);
-        ConstructionLock.Release();
-        return;
-      }
-
-      ConstructionLock.Release();
-      Arduino?.AllowJintOutput(true);
-      active = true;
-    }
-
-    public void Invoke(String functionName, params object[] arguments)
-    {
-      ConstructionLock.Wait(5000);
-      Device.BeginInvokeOnMainThread(() =>
-      {
-        try
+        public void StopRunningProject()
         {
-          Jint().Invoke(functionName, arguments);
+            active = false;
+            Sensors?.Stop();
+            Arduino?.Stop();
+            StopScreen();
+            Network.Signaler.Get().WriteLog("Engine->StopRunningProject: Done.");
         }
-        catch (Jint.Parser.ParserException e)
-        {
-          InteractClient.Network.Signaler.Get().WriteErrorLog(e.Index, e.LineNumber, e.Message, screenToStart);
-        }
-        catch (Jint.Runtime.JavaScriptException e)
-        {
-          InteractClient.Network.Signaler.Get().WriteErrorLog(0, e.LineNumber, e.Message, screenToStart);
-        }
-        catch (Exception e)
-        {
-          InteractClient.Network.Signaler.Get().WriteLog("Engine->Invoke on function " + functionName + ": " + e.Message);
-        }
-      });
-      ConstructionLock.Release();
-    }
 
-    public void SetScreenMessage(string message, bool animate)
-    {
-      if (activePage == null) return;
-      if (activePage is ModelPage) return;
-      Device.BeginInvokeOnMainThread(() =>
-      {
-        (activePage as ConnectedPage)?.SetActivity(message, animate);
-      });
+        public void SetActivePage(ContentPage page)
+        {
+            activePage = page;
+            if (page is ModelPage && ScreenNeedsToStart)
+            {
+                StartScript(screenToStart);
+                ScreenNeedsToStart = false;
+            }
+        }
+
+        public void StartScript(int ID)
+        {
+            Screen screen = Project.Current.GetScreen(ID);
+
+            if (screen == null)
+            {
+                InteractClient.Network.Signaler.Get().WriteLog("Engine->StartScript: screen " + ID + " not found.");
+                ConstructionLock.Release();
+                return;
+            }
+            try
+            {
+
+                Jint().Execute(screen.screenContent.Content);
+                Jint().Invoke("Init");
+                InteractClient.Network.Signaler.Get().WriteLog("Engine->StartScript: screen " + ID + " is now running.");
+            }
+            catch (Jint.Parser.ParserException e)
+            {
+                InteractClient.Network.Signaler.Get().WriteErrorLog(e.Index, e.LineNumber, e.Message, ID);
+                ConstructionLock.Release();
+                return;
+            }
+            catch (Jint.Runtime.JavaScriptException e)
+            {
+                InteractClient.Network.Signaler.Get().WriteErrorLog(0, e.LineNumber, e.Message, ID);
+                ConstructionLock.Release();
+                return;
+            }
+            catch (Exception e)
+            {
+                InteractClient.Network.Signaler.Get().WriteLog("Engine->StartScript: Error on screen " + ID + ":" + e.Message);
+                ConstructionLock.Release();
+                return;
+            }
+
+            ConstructionLock.Release();
+            Arduino?.AllowJintOutput(true);
+            active = true;
+        }
+
+        public void Invoke(String functionName, params object[] arguments)
+        {
+            ConstructionLock.Wait(5000);
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                try
+                {
+                    Jint().Invoke(functionName, arguments);
+                }
+                catch (Jint.Parser.ParserException e)
+                {
+                    InteractClient.Network.Signaler.Get().WriteErrorLog(e.Index, e.LineNumber, e.Message, screenToStart);
+                }
+                catch (Jint.Runtime.JavaScriptException e)
+                {
+                    InteractClient.Network.Signaler.Get().WriteErrorLog(0, e.LineNumber, e.Message, screenToStart);
+                }
+                catch (Exception e)
+                {
+                    InteractClient.Network.Signaler.Get().WriteLog("Engine->Invoke on function " + functionName + ": " + e.Message);
+                }
+            });
+            ConstructionLock.Release();
+        }
+
+        public void SetScreenMessage(string message, bool animate)
+        {
+            if (activePage == null) return;
+            if (activePage is ModelPage) return;
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                (activePage as ConnectedPage)?.SetActivity(message, animate);
+            });
+        }
     }
-  }
 }
--- InteractClient/InteractClient/InteractClient/Network/Service.cs
+++ InteractClient/InteractClient/InteractClient/Network/Service.cs
@@ -12,203 +12,205 @@ using Xamarin.Forms;
 
 namespace InteractClient.Network
 {
-  public class Signaler
-  {
-    private static Signaler instance = null;
-    private HubConnection connection;
-    private IHubProxy proxy;
-    public IHubProxy Proxy { get => proxy; }
-    public ServerList ConnectedServer = null;
-    public bool Connected { get; set; } = false;
-
-    public static Signaler Get()
+    public class Signaler
     {
-      if (instance == null)
-      {
-        instance = new Signaler();
-      }
-      return instance;
-    }
+        private static Signaler instance = null;
+        private HubConnection connection;
+        private IHubProxy proxy;
+        public IHubProxy Proxy { get => proxy; }
+        public ServerList ConnectedServer = null;
+        public bool Connected { get; set; } = false;
 
-    public Signaler()
-    {
-      Connected = false;
-    }
-
-    public async Task ConnectAsync(ServerList server)
-    {
-      if(Connected)
-      {
-        Disconnect();
-      }
-      Connected = true;
-      ConnectedServer = server;
-      connection = new HubConnection("http://" + server.Address + ":" + Constants.TcpPort + "/signalr");
-      connection.Closed += Disconnect;
-      proxy = connection.CreateHubProxy("SignalReceiver");
-      AddMethods();
-
-      try
-      {
-        await connection.Start();
-        Global.UpdatePage(true);
-        await proxy.Invoke("SetName", Settings.Current.Get<String>("UserName"));
-      }
-      catch (Exception e)
-      {
-        Debug.WriteLine("Connection failed: " + e.Message);
-        Connected = false;
-        Global.UpdatePage(false);
-      }
-    }
-
-    public void Disconnect()
-    {
-      ConnectedServer = null;
-      Connected = false;
-
-      if (connection != null)
-      {
-        try
+        public static Signaler Get()
         {
-          connection.Stop();
-          Debug.WriteLine("Connection closed");
-        } catch (NullReferenceException) {}
-      }
+            if (instance == null)
+            {
+                instance = new Signaler();
+            }
+            return instance;
+        }
+
+        public Signaler()
+        {
+            Connected = false;
+        }
+
+        public async Task ConnectAsync(ServerList server)
+        {
+            if (Connected)
+            {
+                Disconnect();
+            }
+            Connected = true;
+            ConnectedServer = server;
+            connection = new HubConnection("http://" + server.Address + ":" + Constants.TcpPort + "/signalr");
+            connection.Closed += Disconnect;
+            proxy = connection.CreateHubProxy("SignalReceiver");
+            AddMethods();
+
+            try
+            {
+                await connection.Start();
+                Global.UpdatePage(true);
+                await proxy.Invoke("SetName", Settings.Current.Get<String>("UserName"));
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Connection failed: " + e.Message);
+                Connected = false;
+                Global.UpdatePage(false);
+            }
+        }
+
+        public void Disconnect()
+        {
+            ConnectedServer = null;
+            Connected = false;
+
+            if (connection != null)
+            {
+                try
+                {
+                    connection.Stop();
+                    Debug.WriteLine("Connection closed");
+                }
+                catch (NullReferenceException) { }
+            }
+        }
+
+        public void GetProjectConfig(Guid ID)
+        {
+            proxy?.Invoke("GetProjectConfig", ID);
+        }
+
+
+        public void GetNextMethod()
+        {
+            proxy?.Invoke("GetNextMethod");
+        }
+
+        public void GetScreen(Guid projectID, int screenID)
+        {
+            proxy?.Invoke("GetScreen", projectID, screenID);
+        }
+
+        public void GetImage(Guid projectID, int imageID)
+        {
+            proxy?.Invoke("GetImage", projectID, imageID);
+        }
+
+        public void GetSoundFile(Guid projectID, int sfID)
+        {
+            proxy?.Invoke("GetSoundFile", projectID, sfID);
+        }
+
+        public void WriteLog(string message)
+        {
+            proxy?.Invoke("Log", message);
+        }
+
+        public void WriteErrorLog(int index, int lineNumber, string message, int resourceID)
+        {
+            proxy?.Invoke("ErrorLog", index, lineNumber, message, resourceID);
+        }
+
+        public void InvokeMethod(string method, params object[] arguments)
+        {
+            proxy?.Invoke("InvokeMethod", method, arguments);
+        }
+
+        public void InvokeMethod(string clientID, string method, params object[] arguments)
+        {
+            proxy?.Invoke("InvokeMethod", clientID, method, arguments);
+        }
+
+        public void StartScreen(string clientID, int screenID)
+        {
+            proxy?.Invoke("StartScreen", clientID, screenID);
+        }
+
+        private void AddMethods()
+        {
+            proxy.On("RequestAcknowledge", () =>
+                proxy.Invoke("Acknowledge")
+            );
+
+            proxy.On("SetCurrentProject", (Guid ID, int Version) =>
+                {
+                    Data.Project.SetCurrent(ID, Version);
+                    Engine.Instance.SetScreenMessage("Loading Project...", true);
+                }
+            );
+
+            proxy.On("SetProjectConfig", (Guid ID, Dictionary<string, string> values) =>
+                Data.Project.List[ID]?.SetConfig(values)
+            );
+
+            proxy.On("SetScreenVersion", (Guid projectID, int screenID, int Version) =>
+                {
+                    Data.Project.List[projectID]?.SetScreenVersion(projectID, screenID, Version);
+                    Engine.Instance.SetScreenMessage("Loading Screens...", true);
+                }
+            );
+
+            proxy.On("StopCurrentScreen", () =>
+                {
+                    Engine.Instance.StopScreen();
+                    Engine.Instance.SetScreenMessage("Screen Stopped.", false);
+                }
+            );
+
+            proxy.On("StopRunningProject", () =>
+            {
+                Engine.Instance.StopRunningProject();
+                WriteLog("Stopping Current Project.");
+            });
+
+            proxy.On("StartScreen", (int ID) =>
+                Engine.Instance.StartScreen(ID)
+            );
+
+            proxy.On("UpdateScreen", (Guid projectID, int ID, string Content) =>
+                Data.Project.List[projectID]?.UpdateScreen(ID, Content)
+            );
+
+            proxy.On("SetImageversion", (Guid projectID, int imageID, int Version) =>
+            {
+                Data.Project.List[projectID]?.SetImageVersion(projectID, imageID, Version);
+                Engine.Instance.SetScreenMessage("Loading Images...", true);
+            });
+
+            proxy.On("UpdateImage", (Guid projectID, int ID, string Content) =>
+            {
+                Data.Project.List[projectID]?.UpdateImage(ID, Content);
+            });
+
+            proxy.On("SetSoundFileVersion", (Guid projectID, int sfID, int Version) =>
+            {
+                Data.Project.List[projectID]?.SetSoundFileVersion(projectID, sfID, Version);
+                Engine.Instance.SetScreenMessage("Loading Sounds...", true);
+            });
+
+            proxy.On("UpdateSoundFile", (Guid projectID, int ID, string Content) =>
+            {
+                Data.Project.List[projectID]?.UpdateSoundFile(ID, Content);
+            });
+
+            proxy.On("AddClient", (string IP, string ID, string Name, bool local) =>
+            {
+                Data.Project.Current.Clients.Add(IP, ID, Name, local);
+                GetNextMethod();
+            });
+
+            proxy.On("InvokeMethod", (string MethodName, object[] arguments) =>
+             {
+                 Engine.Instance.Invoke(MethodName, arguments);
+             });
+
+            proxy.On("CloseConnection", () =>
+            {
+                Disconnect();
+            });
+        }
     }
-
-    public void GetProjectConfig(Guid ID)
-    {
-      proxy?.Invoke("GetProjectConfig", ID);
-    }
-
-
-    public void GetNextMethod()
-    {
-      proxy?.Invoke("GetNextMethod");
-    }
-
-    public void GetScreen(Guid projectID, int screenID)
-    {
-      proxy?.Invoke("GetScreen", projectID, screenID);
-    }
-
-    public void GetImage(Guid projectID, int imageID)
-    {
-      proxy?.Invoke("GetImage", projectID, imageID);
-    }
-
-    public void GetSoundFile(Guid projectID, int sfID)
-    {
-      proxy?.Invoke("GetSoundFile", projectID, sfID);
-    }
-
-    public void WriteLog(string message)
-    {
-      proxy?.Invoke("Log", message);
-    }
-
-    public void WriteErrorLog(int index, int lineNumber, string message, int resourceID)
-    {
-      proxy?.Invoke("ErrorLog", index, lineNumber, message, resourceID);
-    }
-
-    public void InvokeMethod(string method, params object[] arguments)
-    {
-      proxy?.Invoke("InvokeMethod", method, arguments);
-    }
-
-    public void InvokeMethod(string clientID, string method, params object[] arguments)
-    {
-      proxy?.Invoke("InvokeMethod", clientID, method, arguments);
-    }
-
-    public void StartScreen(string clientID, int screenID)
-    {
-      proxy?.Invoke("StartScreen", clientID, screenID);
-    }
-
-    private void AddMethods()
-    {
-      proxy.On("RequestAcknowledge", () =>
-          proxy.Invoke("Acknowledge")
-      );
-
-      proxy.On("SetCurrentProject", (Guid ID, int Version) =>
-          {
-            Data.Project.SetCurrent(ID, Version);
-            Engine.Instance.SetScreenMessage("Loading Project...", true);
-          }
-      );
-
-      proxy.On("SetProjectConfig", (Guid ID, Dictionary<string, string> values) =>
-          Data.Project.List[ID]?.SetConfig(values)
-      );
-
-      proxy.On("SetScreenVersion", (Guid projectID, int screenID, int Version) =>
-          {
-            Data.Project.List[projectID]?.SetScreenVersion(projectID, screenID, Version);
-            Engine.Instance.SetScreenMessage("Loading Screens...", true);
-          }
-      );
-
-      proxy.On("StopCurrentScreen", () =>
-          {
-            Engine.Instance.StopScreen();
-            Engine.Instance.SetScreenMessage("Screen Stopped.", false);
-          }
-      );
-
-      proxy.On("StopRunningProject", () =>
-      {
-        Engine.Instance.StopRunningProject();
-      });
-
-      proxy.On("StartScreen", (int ID) =>
-          Engine.Instance.StartScreen(ID)
-      );
-
-      proxy.On("UpdateScreen", (Guid projectID, int ID, string Content) =>
-          Data.Project.List[projectID]?.UpdateScreen(ID, Content)
-      );
-
-      proxy.On("SetImageversion", (Guid projectID, int imageID, int Version) =>
-      {
-        Data.Project.List[projectID]?.SetImageVersion(projectID, imageID, Version);
-        Engine.Instance.SetScreenMessage("Loading Images...", true);
-      });
-
-      proxy.On("UpdateImage", (Guid projectID, int ID, string Content) =>
-      {
-        Data.Project.List[projectID]?.UpdateImage(ID, Content);
-      });
-
-      proxy.On("SetSoundFileVersion", (Guid projectID, int sfID, int Version) =>
-      {
-        Data.Project.List[projectID]?.SetSoundFileVersion(projectID, sfID, Version);
-        Engine.Instance.SetScreenMessage("Loading Sounds...", true);
-      });
-
-      proxy.On("UpdateSoundFile", (Guid projectID, int ID, string Content) =>
-      {
-        Data.Project.List[projectID]?.UpdateSoundFile(ID, Content);
-      });
-
-      proxy.On("AddClient", (string IP, string ID, string Name, bool local) =>
-      {
-        Data.Project.Current.Clients.Add(IP, ID, Name, local);
-        GetNextMethod();
-      });
-
-      proxy.On("InvokeMethod", (string MethodName, object[] arguments) =>
-       {
-         Engine.Instance.Invoke(MethodName, arguments);
-       });
-
-      proxy.On("CloseConnection", () =>
-      {
-        Disconnect();
-      });
-    }
-  }
 }
